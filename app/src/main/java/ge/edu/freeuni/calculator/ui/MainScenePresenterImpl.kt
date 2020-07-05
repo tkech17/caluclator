f30505a7a7dd0e0930caccb91f5e8eeea112f9b6
tkech17 <tornike.kechakmadze@azry.com>
2020-07-05 16:51:22 +0400

    done

--- app/src/main/java/ge/edu/freeuni/calculator/ui/MainScenePresenterImpl.kt
+++ app/src/main/java/ge/edu/freeuni/calculator/ui/MainScenePresenterImpl.kt
@@ -1,6 +1,13 @@
 package ge.edu.freeuni.calculator.ui
 
+import ge.edu.freeuni.calculator.utils.EvaluateString
+
 class MainScenePresenterImpl(private val view: MainScene.View) : MainScene.Presenter {
+
+    companion object {
+        private val operatorsAndDot: Set<Char> = setOf('+', '-', '/', '*', '.')
+        private val operators: Set<Char> = setOf('+', '-', '/', '*')
+    }
 
     override fun clearInputField() {
         view.clearInputField()
@@ -23,14 +30,57 @@ class MainScenePresenterImpl(private val view: MainScene.View) : MainScene.Prese
 
     override fun evaluate() {
         val inputValue = view.getInputText()
-        clearInputField()
-        val evaluatedValue: String = evaluateExpression(inputValue)
-        view.setEvaluatedValue(evaluatedValue)
+
+        if (!isValidExpression(inputValue)) {
+            view.setEvaluatedValue("Not Valid Expression")
+        } else {
+            clearInputField()
+            val evaluatedValue: String = evaluateExpression(inputValue)
+            view.setEvaluatedValue(evaluatedValue)
+        }
+    }
+
+    private fun isValidExpression(expr: String): Boolean {
+        if (expr.isBlank()) {
+            return true
+        }
+
+        if (operatorsAndDot.contains(expr.first()) || operatorsAndDot.contains(expr.last())) {
+            return false
+        }
+
+        for (i in 0..expr.length - 2) {
+            if (operatorsAndDot.contains(expr[i]) && operatorsAndDot.contains(expr[i + 1])) {
+                return false
+            } else if (expr[i] == '/' && expr[i + 1] == '0') {
+                return false
+            }
+        }
+        return true
     }
 
     private fun evaluateExpression(expression: String): String {
-        return expression
+        var spacedOperatorsExpr = ""
+        for (c in expression) {
+            if (operators.contains(c)) {
+                spacedOperatorsExpr += " $c "
+            } else {
+                spacedOperatorsExpr += c
+            }
+        }
+
+        val result: Double = EvaluateString.evaluate(spacedOperatorsExpr)
+
+        if (isWhole(result)) {
+            return result.toInt().toString()
+        }
+        return result.toString()
     }
+
+    private fun isWhole(value: Double): Boolean {
+        return value - value.toInt() == 0.0
+    }
+
 }
 
 
--- app/src/test/java/ge/edu/freeuni/calculator/ui/MainScenePresenterTest.kt
+++ app/src/test/java/ge/edu/freeuni/calculator/ui/MainScenePresenterTest.kt
@@ -1,6 +1,7 @@
 package ge.edu.freeuni.calculator.ui
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 
@@ -60,18 +61,80 @@ internal class MainScenePresenterTest {
     }
 
     @Test
-    fun onEvaluate() {
+    fun testEvaluateWhenNotValidExpression() {
+        val expectedValue = "Not Valid Expression"
+
         view = mock(MainScene.View::class.java)
             .also { doNothing().`when`(it).clearInputField() }
-            .also { `when`(it.getInputText()).thenReturn("1+1") }
 
         `when`(view.setEvaluatedValue(anyString())).thenAnswer {
-            assertEquals("1+1", it.arguments[0])
+            assertEquals(expectedValue, it.arguments[0])
         }
-
 
         presenter = mainScenePresenter(view)
 
+        `when`(view.getInputText()).thenReturn("+1")
+        presenter.evaluate()
+
+        `when`(view.getInputText()).thenReturn("1+")
+        presenter.evaluate()
+
+        `when`(view.getInputText()).thenReturn("1+1+*")
+        presenter.evaluate()
+
+        `when`(view.getInputText()).thenReturn("1/0")
+        presenter.evaluate()
+
+        `when`(view.getInputText()).thenReturn("1..0")
+        presenter.evaluate()
+
+        `when`(view.getInputText()).thenReturn("1.")
+        presenter.evaluate()
+    }
+
+
+    @Test
+    fun testEvaluateWhenValidExpression1() {
+        view = mock(MainScene.View::class.java)
+            .also { doNothing().`when`(it).clearInputField() }
+
+        `when`(view.setEvaluatedValue(anyString())).thenAnswer {
+            assertEquals("6", it.arguments[0])
+        }
+
+        presenter = mainScenePresenter(view)
+
+        `when`(view.getInputText()).thenReturn("1 + 1 * 10 / 2")
+        presenter.evaluate()
+    }
+
+    @Test
+    fun testEvaluateWhenValidExpression2() {
+        view = mock(MainScene.View::class.java)
+            .also { doNothing().`when`(it).clearInputField() }
+
+        `when`(view.setEvaluatedValue(anyString())).thenAnswer {
+            assertTrue((it.arguments[0] as String).startsWith("3.3333"))
+        }
+
+        presenter = mainScenePresenter(view)
+
+        `when`(view.getInputText()).thenReturn("10 / 3")
+        presenter.evaluate()
+    }
+
+    @Test
+    fun testEvaluateWhenValidExpression3() {
+        view = mock(MainScene.View::class.java)
+            .also { doNothing().`when`(it).clearInputField() }
+
+        `when`(view.setEvaluatedValue(anyString())).thenAnswer {
+            assertEquals("5", it.arguments[0])
+        }
+
+        presenter = mainScenePresenter(view)
+
+        `when`(view.getInputText()).thenReturn("10 / 2")
         presenter.evaluate()
     }
 
